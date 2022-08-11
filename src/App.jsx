8fa0f7ff2ee3a9c7894255b24b470080cd598d38
Thales <thales.souz@outlook.com>
2022-08-11 10:58:29 -0300

    docs: Conectar ao repositorio git

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
           <img src={ reactLogo } className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React AAAAAA</h1>
       <div className="card">
         <button onClick={ () => setCount((counter) => counter + 1) } type="button">
           count is
