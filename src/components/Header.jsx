4812d434316cb5fff2b14efefea56571cfc8ac38
Thales <thales.souz@outlook.com>
2022-08-11 12:06:17 -0300

    build: Começando o Header e definido a imagem de fundo

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header-container">
+      <h2 className="logo">THALES</h2>
+    </header>
+  );
+}
+
+export default Header;
