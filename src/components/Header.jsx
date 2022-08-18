05bf6f64471d543c7c836c1a55ba48a5fe9b7d9e
Thales <thales.souz@outlook.com>
2022-08-17 21:02:35 -0300

    fix: reformulando

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,15 @@ import './Header.css';
 
 function Header() {
   return (
-    <header className="header-container">
-      <h2 className="logo">Thales Chagas</h2>
+    <header className="header">
+      <div className="header-container">
+        <img src="src/images/logo.png" width="70px" alt="logo" />
+        <section className="text-header">
+          <h2>Thales Chagas</h2>
+          <h5>Fullstack Web Developer Student</h5>
+        </section>
+      </div>
+      <div className="fade" />
     </header>
   );
 }
