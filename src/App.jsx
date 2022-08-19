d5e768cf324f515cdc49ca58006641580c1045b9
Thales <thales.souz@outlook.com>
2022-08-19 16:49:09 -0300

    build: começando initial page

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,17 @@ function App() {
   return (
     <main className="page">
       <Header />
-      <h1 className="main-text">Hello There, I&apos;m Thales!</h1>
+      <h1 className="title">Web Developer</h1>
+      <article className="main-text">
+        <p className="title-name">THALES SOUZA CHAGAS</p>
+        <h1 className="introduction">
+          Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Eum asperiores
+          sint aperiam iusto quasi dicta error officia.
+          Maxime obcaecati culpa inventore repellat quasi
+          facilis dicta debitis architecto eveniet, dolore nisi.
+        </h1>
+      </article>
     </main>
   );
 }
