c25a6c0cced1e11bb4785136d1875a9467be7f25
Thales <thales.souz@outlook.com>
2022-08-19 12:52:17 -0300

    build: reformulado cor, criado menu hamburguer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+
 import './Header.css';
+import Menu from './Menu';
 
 function Header() {
   return (
     <header className="header">
       <div className="header-container">
-        <img src="src/images/logo.png" width="70px" alt="logo" />
-        <section className="text-header">
-          <h2>Thales Chagas</h2>
-          <h5>Fullstack Web Developer Student</h5>
+        <section className="logo">
+          <img src="src/images/logo.png" width="70px" alt="logo" />
+          <section className="text-header">
+            <h1>Thales Chagas</h1>
+            <h4>Fullstack Web Developer Student</h4>
+          </section>
         </section>
+        <Menu />
       </div>
       <div className="fade" />
     </header>
