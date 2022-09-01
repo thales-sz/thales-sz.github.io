4d3d68701cb65897fd61d529c14659fd5f3e4593
Thales <thales.souz@outlook.com>
2022-09-01 15:11:36 -0300

    feat: finished projects label

--- src/components/ProjectsLabel.jsx
+++ src/components/ProjectsLabel.jsx
@@ -6,7 +6,7 @@ import Project from './Project';
 export default function ProjectsLabel() {
   return (
     <div className="projects-container">
-      <h1 className="project-title">PROJECTS</h1>
+      <h1 className="title-name">SELECTED PROJECTS</h1>
       <section className="projects">
         {data.map((project, i) => <Project key={ i } data={ project } id={ i } />)}
       </section>
