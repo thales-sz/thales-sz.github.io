a32e68e7018ba3113d83807157551490dc305440
Thales <thales.souz@outlook.com>
2022-09-05 14:55:26 -0300

    feat: about me ideias non finished

--- src/components/AboutMe/index.jsx
+++ src/components/AboutMe/index.jsx
@@ -7,8 +7,17 @@ function AboutMe() {
       <h1 className="title-name">ABOUT ME</h1>
       <h2 className="second-title">A FEW WORDS ABOUT ME</h2>
       <article className="text-about-me">
-        I&apos;m Thales Souza Chagas a 21 years old
-        frontend developer and I&apos;m currently learning backend development
+        I&apos;m Thales Souza Chagas, a junior frontend developer who
+        always focuses on learning something new through studies and projects in
+        practice. I&apos;m passionate
+        about making things happen through programming.
+        Programming for me is bringing your ideas to life...
+        It&apos;s amazing...
+        <p className="second-text">
+          I discovered myself in programming in 2016 where
+          I made a significant technical course in electronics and programmed an
+          arduino using C++ among other things, since then I&apos;m love with technology
+        </p>
       </article>
       <article>images</article>
     </section>
--- src/components/ProjectsLabel.jsx
+++ src/components/ProjectsLabel.jsx
@@ -7,6 +7,14 @@ export default function ProjectsLabel() {
   return (
     <div className="projects-container" id="projects">
       <h1 className="title-name">SELECTED PROJECTS</h1>
+      <h2 className="text-projects">
+        These projects were made during my
+        development course at
+        {' '}
+        <a href="https://www.betrybe.com/" target="_blank" rel="noreferrer">Trybe.</a>
+        {' '}
+        See some of them...
+      </h2>
       <section className="projects">
         {data.map((project, i) => <Project key={ i } data={ project } id={ i } />)}
       </section>
