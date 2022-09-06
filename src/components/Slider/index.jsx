bb53ded575257e059b48e567ea4745cbc92121a5
Thales <thales.souz@outlook.com>
2022-09-06 15:38:21 -0300

    fix: error Swiper lib

---
+++ src/components/Slider/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Slider.css';
+
+// import { BiLeftArrow, BiRightArrow } from 'react-icons/bi';
+
+function Slider() {
+  return (
+    <div className="container">
+      <div className="slider">
+        <img src="https://i.im.ge/2022/09/06/OZlnsc.eu-primeira-ft.png" alt="eu" />
+        <img src="https://i.im.ge/2022/09/06/OZrqZh.gym.png" alt="gym" />
+        <img src="https://i.im.ge/2022/09/06/OZlpY0.euepedrin.png" alt="euepedrin" />
+        <img src="https://i.im.ge/2022/09/06/OZlvBT.bnr.png" alt="bnr" />
+      </div>
+    </div>
+  );
+}
+
+export default Slider;
