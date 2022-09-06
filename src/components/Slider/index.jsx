a420b7a35e0bdb173b76bcda9ec14dc6b64e68cc
Thales <thales.souz@outlook.com>
2022-09-06 17:44:52 -0300

    fix: vite build error try 3

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper.min.css';
-import 'swiper/modules/pagination/pagination.min.css';
 import 'swiper/swiper-bundle.css';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Navigation, Scrollbar } from 'swiper';
 
 import './Slider.css';
 
@@ -11,15 +9,16 @@ function Slider() {
   return (
     <div className="container">
       <Swiper
-        modules={ [Navigation, Pagination, Scrollbar, A11y] }
+        modules={ [Navigation, Scrollbar] }
         spaceBetween={ 50 }
         slidesPerView={ 1 }
-        navigation={ { clickable: true } }
-        pagination={ { clickable: true } }
         scrollbar={ { draggable: true } }
         loop
-        onSwiper={ (swiper) => console.log(swiper) }
+        autoplay
+        speed={ 800 }
+        onSwiper={ (swi) => console.log(swi) }
         onSlideChange={ () => console.log('slide change') }
+        className="swiper"
       >
         <SwiperSlide className="swiper-slide">
           <img src="https://i.im.ge/2022/09/06/OZlnsc.eu-primeira-ft.png" alt="eu" width="400px" />
@@ -31,7 +30,7 @@ function Slider() {
           <img src="https://i.im.ge/2022/09/06/OZlpY0.euepedrin.png" alt="euepedrin" width="400px" />
         </SwiperSlide>
         <SwiperSlide className="swiper-slide">
-          <img src="https://i.im.ge/2022/09/06/OZlvBT.bnr.png" alt="bnr" width="400px" />
+          <img src="https://i.im.ge/2022/09/06/OZlvBT.bnr.png" alt="bn" width="400px" />
         </SwiperSlide>
       </Swiper>
     </div>
