31a3792e6cc95acc85816c4151268c31310e631b
Thales <thales.souz@outlook.com>
2022-09-07 18:39:48 -0300

    feat: finalizado

--- src/components/ContactMe/index.jsx
+++ src/components/ContactMe/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './contactMe.css';
 import { ImArrowRight } from 'react-icons/im';
+import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
 
 function ContactMe() {
   return (
@@ -13,9 +14,49 @@ function ContactMe() {
           <ImArrowRight size="40px" />
         </div>
       </a>
-      <nav className="information-nav">
-        Contact information —
-      </nav>
+      <hr />
+      {/* <h1 className="subtitle">Get in Touch</h1> */}
+      <div className="info-container">
+        <nav className="information-nav">
+          <h1>Contact information —</h1>
+          <p>Feel free to reach out to me any time.</p>
+          <div className="email-phone">
+            <p>
+              Email:
+              {' '}
+              <a href="mailto:thales.souz@outlook.com">
+                thales.souz@outlook.com
+              </a>
+            </p>
+            <p>
+              Phone:
+              {' '}
+              <span>
+                +55 (24) 999035459
+              </span>
+            </p>
+          </div>
+        </nav>
+        <nav className="nav-links">
+          <h1>Links —</h1>
+          <a className="link-nav" href="https://github.com/thales-sz" rel="noreferrer" target="_blank">
+            <FaGithubSquare size="60px" />
+            GitHub
+          </a>
+          <a className="link-nav" href="https://www.linkedin.com/in/thales-sz/" rel="noreferrer" target="_blank">
+            <FaLinkedin size="60px" />
+            Linkedin
+          </a>
+          <a className="download" href="https://drive.google.com/u/0/uc?id=1CDaHxOfgtYMPF35Z28KdvKBAIlTPkMU3&export=download">Download CV</a>
+        </nav>
+        <nav className="social-nav">
+          <h1>Follow me on —</h1>
+          <a href="https://www.instagram.com/thales.sc/" rel="noreferrer" target="_blank">Instagram</a>
+          <a href="https://twitter.com/thalessouz1" rel="noreferrer" target="_blank">Twitter</a>
+          <a href="https://www.hackerrank.com/thales_souz" rel="noreferrer" target="_blank">Hackerrank</a>
+          <a href="https://www.linkedin.com/in/thales-sz/" rel="noreferrer" target="_blank">Linkedin</a>
+        </nav>
+      </div>
     </div>
   );
 }
